8c84633f8b9b0f6d5f4bae6c675248c21268c8fd
tharunkumar453 <23021a0453@jntucek.ac.in>
2026-03-19 18:39:03 +0530

    Update urls.py

--- backend/compile/urls.py
+++ backend/compile/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 urlpatterns=[
     path("Submit/", submit.as_view(), name="submit"),
-    path("comoile/" submit.as_view(), name="compilation"),
+    path("compile/" ,submit.as_view(), name="compilation"),
     path("Dashboard/", UserDashboardView.as_view(), name="dashboard-list"),
     path("TotalSubmissions/",TotalSubmissions.as_view(),name="count"),
     path("Leaderboard/", LeaderBoard.as_view(), name="leaderboard"),
